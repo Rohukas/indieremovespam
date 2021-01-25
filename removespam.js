4790ec47c6040fa5213b272303bb66b3d802d9c8
Robi <32801664+Rohukas@users.noreply.github.com>
2021-01-25 12:32:29 +0200

    Create removespam.js

---
+++ removespam.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         IH Spam Removal
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Removes posts that contain chinese characters from the 'newest' tab in IndieHackers.
+// @author       Robi
+// @match        https://www.indiehackers.com/newest
+// @grant        none
+// ==/UserScript==
+
+window.onload = (event) => {
+    setInterval(()=>{
+    for (let doc of document.getElementsByClassName('feed-item')){
+        if (/[\u4e00-\u9FFF]/.test(doc.innerText)){
+            doc.remove()
+            console.log("Removed spam.")
+
+        }
+    }
+    },500)
+};
+
